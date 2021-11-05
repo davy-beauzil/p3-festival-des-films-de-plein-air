8f7f5e12eae91a41b2eee8e88a6770e946e9bca8
davy-beauzil <davy.beauzil72@gmail.com>
2021-11-05 18:18:29 +0100

    feat(index.html): ajout formulaire newsletter

--- public/js/show-nav.js
+++ public/js/show-nav.js
@@ -1,28 +1,53 @@
-var btn = document.querySelector('div.btn_bars');
-var nav = document.querySelector('nav.nav_mobile');
+var btn = document.querySelector('div.btn_bars').classList;
+var nav = document.querySelector('nav.nav_mobile').classList;
+var newsletter = document.querySelector('.newsletter_form').classList;
 
-btn.addEventListener('click', function(e){
-    e.preventDefault();
-    nav_classes = nav.classList;
-    btn_classes = btn.classList;
-    if(nav_classes.contains('hidden')){
-        nav.classList.remove('hidden');
-        nav.classList.add('block');
+function barsButton(){
 
-        if(btn_classes.contains('not-active')){
-            console.log('test test')
-            btn.classList.remove('not-active');
-            btn.classList.add('active');
-            document.documentElement.style.overflow = 'hidden';
-        }
+    if(newsletterIsShowed()){
+        newsletter.remove('flex');
+        newsletter.add('hidden');
+        btn.remove('active');
+        btn.add('not-active');
+        btn.add('lg:hidden');
+    }else if (mobileNavigationIsShowed()){
+        nav.remove('block');
+        nav.add('hidden');
+        btn.remove('active');
+        btn.add('not-active');
     }else{
-        nav.classList.remove('block');
-        nav.classList.add('hidden');
-
-        if(btn_classes.contains('active')){
-            btn.classList.remove('active');
-            btn.classList.add('not-active');
-            document.documentElement.style.overflow = 'visible';
-        }
+        nav.add('block');
+        nav.remove('hidden');
+        btn.add('active');
+        btn.remove('not-active');
     }
-})
+}
+
+function showNewsletterForm(){
+
+    if(!newsletterIsShowed()){
+        newsletter.remove('hidden');
+        newsletter.add('flex');
+        btn.remove('not-active');
+        btn.remove('lg:hidden');
+        btn.add('active');
+    }
+}
+
+function newsletterIsShowed(){
+
+    if(newsletter.contains('hidden')){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function mobileNavigationIsShowed(){
+
+    if(nav.contains('hidden')){
+        return false;
+    } else {
+        return true;
+    }
+}
